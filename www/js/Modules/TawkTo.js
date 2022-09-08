6d9f1f48781196c407e39175dbac89ade2eba257
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2022-09-08 14:17:50 +0800

    SAFE COPY

--- www/js/Modules/TawkTo.js
+++ www/js/Modules/TawkTo.js
@@ -1,21 +1,51 @@
 
 
 
-function TAWKTO_CHAT_INIT(_USER_DATA_){
-	
-	var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 
-	Tawk_API.visitor = {
-		name : _USER_DATA_["name"],
-		email : _USER_DATA_["email"]
-	};
+function TAWKTO_CHAT_INIT(UISER__){
+			// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+			// Tawk_API.visitor = {
+			// 	name : UISER__["name"],
+			// 	email : UISER__["email"]
+			// };
 
-	(function(){
-		var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-		s1.async=true;
-		s1.src='https://embed.tawk.to/62f9c9c554f06e12d88ea549/1gafrob5j';
-		s1.charset='UTF-8';
-		s1.setAttribute('crossorigin','*');
-		s0.parentNode.insertBefore(s1,s0);
-	})();
+			// Tawk_API.onLoad = function(){
+			// 	Tawk_API.setAttributes({
+			// 		name : UISER__["name"],
+			// 		email : UISER__["email"]
+			// 		// 'hash' : 'hash-value'
+			// 	}, function (error) {
+
+			// 	});
+
+			// };
+			
+
+			// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+			// s1.async=true;
+			// s1.src='https://embed.tawk.to/62f9c9c554f06e12d88ea549/1gafrob5j';
+			// s1.charset='UTF-8';
+			// s1.setAttribute('crossorigin','*');
+			// s0.parentNode.insertBefore(s1,s0);
+			
+
+			// Tawk_API.onLoad = function(){
+			// 	Tawk_API.setAttributes({
+			// 		name : UISER__["name"],
+			// 		email : UISER__["email"]
+			// 		// 'hash' : 'hash-value'
+			// 	}, function (error) {
+
+			// 	});
+
+			// };
+			// alert(JSON.stringify(Tawk_API))
+		// ---------------------------------------------------------------------
+
 }
+
+
+
+
+
+		
